322598e514433d9d2692390b62462e413c970c78
Telemachusghost <34588681+Telemachusghost@users.noreply.github.com>
2018-05-08 18:34:23 -0700

    Update readIMDBfile.py

--- readIMDBfile.py
+++ readIMDBfile.py
@@ -1,12 +1,20 @@
+"""
+This script reads in an imdb database file and parses it putting it into a database for further educational
+use
+Derick Falk
+"""
+
 import pandas as pd
 import json, requests
 from MySQLdb import *
 
 movies = {}
 
-cnx = connect(user='testprojects', password='Testing123!',host='den1.mysql4.gear.host',database='testprojects')
+# Connects to your database replace with you username, password, and hostname
+cnx = connect(user=<user>, password=<password>,host=<hostname>,database=<database>)
 cursor = cnx.cursor()
 
+# Method that saves a movie to the database column names and structure may vary
 def savetodb(dct):
 	add_movie = 'INSERT INTO movies \
 	(title, year, runtime, genre, rating) \
@@ -18,43 +26,34 @@ def savetodb(dct):
 	 	movies[dct]['rating'])
 	cursor.execute(add_movie)
 	cnx.commit()
+
+# Used pandas to read the tsvs	
 df = pd.read_table('data.tsv',low_memory=False)
 dfr = pd.read_table('data_ratings.tsv', low_memory=False)
 
+# Convert the columns into tuples
 dftuples =  df.itertuples()
 
-
+# Breaks up the datafile and makes a dictionary based on id
 for i in dftuples:
 	try:
-		#print(f"id: {i[1]} title: {i[3]} year:{i[6]} runtime: {i[8]} genres: {i[-1]}")
 		movies[i[1]] = {'title':i[3],'year':int(i[6]),'runtime':int(i[8]),'genre(s)':i[-1]}
 	except:
 		continue
 
-
+# Adds the ratings from a seperate tsv
 for i in dfr.itertuples():
 	try:
 		movies[i[1]]['rating'] = float(i[2])
 	except:
 		continue
 	
-	
+# Goes through and saves the movies to a database
 for i in movies:
 	try:
 		print(i)
 		savetodb(i)
 	except:
 		continue
-#sorted_df = df.sort_index()
-#print(sorted_df)
-
-
-
-
-
-"""
-tags = ['Title','Year','Rated','Released',
-       'Runtime','Director','Writer', 'Actors','Country','Ratings']
-"""
 
 
